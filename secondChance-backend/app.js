49cceec4c0543a6f4a5f4cdc4d104f700fc24784
aidilfitra08 <aidil.gitu1@gmail.com>
2025-03-07 14:14:41 -0500

    fix linting

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,16 +1,15 @@
-/*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
-const path = require('path');
+/* jshint esversion: 8 */
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const pinoLogger = require('./logger')
+const path = require('path')
 
-const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
+const connectToDatabase = require('./models/db')
+// const { loadData } = require("./util/import-mongo/index");
 
-
-const app = express();
-app.use("*",cors());
+const app = express()
+app.use('*', cors())
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
